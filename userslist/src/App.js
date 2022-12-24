c7e3ecafb063ea6bff426cae1265f4bff6ff26b0
Suryoday18 <suryoday.yadav@transerve.com>
2022-12-24 15:35:42 +0530

    Adding Post API call

--- userslist/src/App.js
+++ userslist/src/App.js
@@ -7,9 +7,12 @@ const url = 'https://reqres.in/api/users'
 function App() {
   const [loading, setLoading] = useState(true)
   const [users, setUsers] = useState([])
- 
+ const [email, setEmail] = useState('')
+ const [firstName, setFirstName] = useState('')
+ const [lastName, setLastName] = useState('')
 
-  const fetchJobs = async () => {
+
+  const fetchUsers = async () => {
     const reponse = await fetch(url)
     const newUsers = await reponse.json()
     
@@ -17,8 +20,25 @@ function App() {
     setLoading(false)
     
   }
+  
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    let obj = {}
+    obj.email = email
+    obj.first_name = firstName
+    obj.last_name = lastName
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(obj)
+  };
+  await fetch(url, requestOptions)
+  .then(response => response.json())
+  .then(data => setUsers(data));
+  }
   useEffect(() => {
-    fetchJobs()
+    fetchUsers()
   }, [])
   if (loading) {
     return (
@@ -37,23 +57,28 @@ function App() {
         <th>Id</th>
         <th>Name</th>
         <th>Email</th>
-    </tr>
+      </tr>
     
     {users.data.map((user, index) => {
       const {id, email, first_name, last_name, avatar} = user
       return(
         <tr key={index}>
-        <th><img src={avatar}/></th>
-        <th>{id}</th>
-        <th>{first_name + " " + last_name}</th>
-        <th><a href={email}>{email}</a></th>
+        <td><img src={avatar}/></td>
+        <td>{id}</td>
+        <td>{first_name + " " + last_name}</td>
+        <td><a href={email}>{email}</a></td>
         </tr>
       )
      
     })}
+    <tr>
+        <td><button className='addbutton' onClick={handleSubmit}>Add User</button></td>
+        <td><input type='text' value={firstName} className='input-field' onChange={(e) => setFirstName(e.target.value)} placeholder='Enter First Name'/></td>
+        <td><input type='text' value={lastName} className='input-field' onChange={(e) => setLastName(e.target.value)} placeholder='Enter last Name' /></td>
+        <td><input type='email' value={email} className='input-field' onChange={(e) => setEmail(e.target.value)} placeholder='Enter Email'/></td>
+    </tr>
     </table>
     </div>
-    <button>Add User</button>
     </>
     )
   }
